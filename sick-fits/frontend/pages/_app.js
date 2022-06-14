6a18156d57bf98ea9969c07de34f33bae096342f
nikitakonan <nikitakonan1991@gmail.com>
2022-06-14 17:17:13 +0300

    Added ns progress

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,6 +1,13 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import PropTypes from 'prop-types';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
