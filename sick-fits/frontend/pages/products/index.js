5f06b704534e107b18f52c7a6ce13a20e3698279
nikitakonan <nikitakonan1991@gmail.com>
2022-06-19 15:41:00 +0300

    Added pagination

---
+++ sick-fits/frontend/pages/products/index.js
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router';
+import Pagination from '../../components/Pagination';
+import Products from '../../components/Products';
+
+export default function ProductsPage() {
+  const { query } = useRouter();
+  const page = parseInt(query.page) || 1;
+
+  return (
+    <div>
+      <Pagination page={page} />
+      <Products page={page} />
+      <Pagination page={page} />
+    </div>
+  );
+}
+
+ProductsPage.propTypes = {
+  // param: PropTypes.object,
+};
