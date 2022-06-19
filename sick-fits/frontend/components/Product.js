834bbaa4c456143422ad13e524dbe21986354f7b
nikitakonan <nikitakonan1991@gmail.com>
2022-06-19 10:02:51 +0300

    Added products

---
+++ sick-fits/frontend/components/Product.js
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import formatMoney from '../lib/formatMoney';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+import Title from './styles/Title';
+
+export default function Product({ product }) {
+  return (
+    <ItemStyles>
+      <img
+        src={product?.photo?.image?.publicUrlTransformed}
+        alt={product.name}
+      />
+      <Title>
+        <Link href={`/product/${product.id}`}>{product.name}</Link>
+      </Title>
+      <PriceTag>{formatMoney(product.price)}</PriceTag>
+      <p>{product.description}</p>
+      {/* TODO: Add buttons to edit and delte item */}
+    </ItemStyles>
+  );
+}
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+    description: PropTypes.string,
+    price: PropTypes.number,
+    photo: PropTypes.shape({
+      image: PropTypes.shape({
+        publicUrlTransformed: PropTypes.string,
+      }),
+    }),
+  }),
+};
