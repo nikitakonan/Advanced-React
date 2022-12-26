3644ed313c14b26e986c991c9c02d0d4fe2546de
nikitakonan <nikitakonan1991@gmail.com>
2022-12-26 15:30:09 +0400

    Try BEM styling

--- sick-fits/frontend/components/Product.js
+++ sick-fits/frontend/components/Product.js
@@ -3,30 +3,31 @@ import PropTypes from 'prop-types';
 import formatMoney from '../lib/formatMoney';
 import AddToCart from './AddToCart';
 import DeleteProduct from './DeleteProduct';
-import ItemStyles from './styles/ItemStyles';
-import PriceTag from './styles/PriceTag';
-import Title from './styles/Title';
+import styles from './styles/product.module.css';
 
 export default function Product({ product }) {
   return (
-    <ItemStyles>
+    <div className={styles['product-item']}>
       <img
+        className={styles['product-item__img']}
         src={product?.photo?.image?.publicUrlTransformed}
         alt={product.name}
       />
-      <Title>
+      <h3 className={styles['product-item__title']}>
         <Link href={`/product/${product.id}`}>{product.name}</Link>
-      </Title>
-      <PriceTag>{formatMoney(product.price)}</PriceTag>
-      <p>{product.description}</p>
-      <div className="buttonList">
+      </h3>
+      <span className={styles['product-item__price-tag']}>
+        {formatMoney(product.price)}
+      </span>
+      <p className={styles['product-item__desc']}>{product.description}</p>
+      <div className={styles['product-item__button-list']}>
         <Link href={{ pathname: `/update`, query: { id: product.id } }}>
           Edit ✏️
         </Link>
         <AddToCart id={product.id} />
         <DeleteProduct id={product.id}>Delete ❌</DeleteProduct>
       </div>
-    </ItemStyles>
+    </div>
   );
 }
 
